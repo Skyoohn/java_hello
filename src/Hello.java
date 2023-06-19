8c7108fdd4a27daa94d47867160596717ae1d850
DESKTOP-4QLEUQH\wagr <naver@naver.com>
2023-06-19 15:12:48 +0900

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("I am a programer.");
+		System.out.println("Hello World!");
+		System.out.println(" *");
+		System.out.println("***");
+	}
+
+}
